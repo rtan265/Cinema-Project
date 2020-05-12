f03a0e2cd0da52f5dbc774e9baa0612952743afc
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-12 16:53:33 +1200

    working so far - links work!

---
+++ client/src/pages/MoviesList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MoviesList extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>In this page you'll see a list of the movies</p>
+            </div>
+        )
+    }
+}
+
+export default MoviesList
---
+++ client/src/pages/MoviesUpdate.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MoviesUpdate extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>In this page you'll see the form to update the movies</p>
+            </div>
+        )
+    }
+}
+
+export default MoviesUpdate
