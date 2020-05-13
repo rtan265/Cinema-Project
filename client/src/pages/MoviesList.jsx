e37e0dfa2dbd8db4d6185e41145bf6697ba99204
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-13 21:14:49 +1200

    Completed, there are some minor issues with updating the movie. this needs to be tracked

--- client/src/pages/MoviesList.jsx
+++ client/src/pages/MoviesList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactTable from 'react-table-6';
 import api from '../api';
 
@@ -9,6 +9,47 @@ import 'react-table-6/react-table.css';
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
 `
+
+const Update = styled.div`
+    color: #ef9b0f;
+    cursor: pointer;
+`
+
+const Delete = styled.div`
+    color: #ff0000;
+    cursor: pointer;
+`
+
+class UpdateMovie extends Component {
+    updateUser = event => {
+        event.preventDefault();
+
+        window.location.href = `/movies/update/${this.props.id}`
+    }
+
+    render() {
+        return <Update onClick = {this.updateUser}>Update</Update>
+    }
+}
+
+class DeleteMovie extends Component{
+    deleteUser = event => {
+        event.preventDefault()
+
+        if (
+            window.confirm(
+                `Do you want to delete the movie ${this.props.id} permanently?`,
+            )
+        ) {
+            api.deleteMovieById(this.props.id)
+            window.location.reload()
+        }
+    }
+    
+    render(){
+        return <Delete onClick = {this.deleteUser}>Delete</Delete>
+    }
+}
 
 class MoviesList extends React.Component {
     constructor(props){
@@ -57,6 +98,28 @@ class MoviesList extends React.Component {
                 Header: 'Time',
                 accessor: 'time',
                 Cell: props => <span>{props.value.join(' / ')}</span>,
+            },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props){
+                    return(
+                        <span>
+                            <DeleteMovie id={props.original._id}/>
+                        </span>
+                    )
+                },
+            },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props){
+                    return(
+                        <span>
+                            <UpdateMovie id = {props.original._id}/>
+                        </span>
+                    )
+                },
             },
         ]
 
