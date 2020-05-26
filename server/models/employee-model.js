c2c98f8e76444d9564657be3adb814b2bf30c53f
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-26 16:43:46 +1200

    Added new Schema -> employee-model.js

---
+++ server/models/employee-model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Employee = new Schema(
+    {
+        name: { type: String, required: true},
+        location: { type: String, required: true},
+        startDate: { type: Date, required: true},
+        salary: { type: Number, required: true},
+    },
+    { timestamps: true },
+)
+
+module.export = mongoose.model('employees', Employee)
