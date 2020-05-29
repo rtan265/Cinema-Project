28f821b33079d8d8423823a2829441f31ddb21f7
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-29 23:09:03 +1200

    Added createEmplyee to ctrler

--- server/routes/employee-router.js
+++ server/routes/employee-router.js
@@ -3,3 +3,5 @@ const express = require('express')
 const EmployeeCtrl = require('../controllers/employee-ctrl')
 
 const router = express.Router()
+
+router.post('/employee', EmployeeCtrl.createEmployee) // work on this first.
