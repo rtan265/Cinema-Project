b3f804efce6ff9548e01d8a26b0c4cb6c590fb6c
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-31 23:29:26 +1200

    added get employee id

--- server/routes/employee-router.js
+++ server/routes/employee-router.js
@@ -7,3 +7,4 @@ const router = express.Router()
 router.post('/employee', EmployeeCtrl.createEmployee) // work on this first.
 router.put('employee/:id', EmployeeCtrl.updateEmployee) // next
 router.delete('/employee/:id',EmployeeCtrl.deleteMovie) // Delete data
+router.get('/employee/:id', EmployeeCtrl.getEmployeeById)
